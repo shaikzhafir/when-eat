ce5a1f423b48ff85b3c46b598400e9f3fcf71708
bobross96 <zadus96@gmail.com>
2021-04-13 13:53:04 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+const puppeteer = require('puppeteer');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //puppeter code and  extract the timings 
+  let maghribTime = 'lala'
+  let subuhTime
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.muis.gov.sg/');
+    await page.screenshot({path: 'example.png'});
+    const subuhTime = await page.$eval('#PrayerTimeControl1_Subuh', span => span.textContent)
+    const maghribTime = await page.$eval('#PrayerTimeControl1_Maghrib', span => span.textContent)
+    res.render('index', 
+  { title: 'When to eat',
+    subuhTime : subuhTime,
+    maghribTime : maghribTime
+    });
+    await browser.close();
+  })();
+
+  
+  
+});
+
+module.exports = router;
